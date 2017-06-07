ba0f46f4cf877b853e662d54421dc8d93486f1e6
Jerome Chery <jerome.chery@nrco.fr>
2017-06-07 14:07:47 +0200

    Forçage output

--- Animate.bat
+++ Animate.bat
@@ -1,1 +1,2 @@
-"C:\Program Files\Blender Foundation\Blender\blender.exe" -b Map.blend -P scripts/prepare.py -- %1 %2 %3
+"C:\Program Files\Blender Foundation\Blender\blender.exe" -b Map.blend -o //vid\K4vX_# -P scripts/prepare.py -- %1 %2 %3
+move vid\K4vX_*.avi vid\K4vX.avi  
